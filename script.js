51d85b13b490b95a96c8f00836dec3153a2cdd09
KlemensNa <klemens.naue@gmail.com>
2023-06-29 23:07:41 +0200

    allGenButtons

--- script.js
+++ script.js
@@ -1,17 +1,65 @@
-async function loadPokemon(j, k){
-    let loadedAmount = j+k;
-    for (let i = j; i < loadedAmount; i++) {
+async function loadPokemon(){
+    
+    for (let i = 1; i <= 1010; i++) {
         
         let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         let response = await fetch(url);
         let responseAsJson = await response.json();
         buildCard(responseAsJson);
+        activateFirstGen();
+        buildLoadingDisplay(i);
+        
     }
 
-    createloadFiftyMoreBtn(loadedAmount);
-    createloadHundredBtn(loadedAmount);
-    createloadTwoHundredBtn(loadedAmount);
+    // createloadFiftyMoreBtn(loadedAmount);
+    // createloadHundredBtn(loadedAmount);
+    // createloadTwoHundredBtn(loadedAmount);
 }
+
+function loadGenButton(i){
+    let GenButtonContainer = document.getElementById('genButtonContainer');
+    if(i === 252){
+        GenButtonContainer.innerHTML += /*html*/`
+            <button onclick="activateChoosenGen(152, 251)">Gen2</button>
+        `
+    }
+    if(i === 386){
+        GenButtonContainer.innerHTML += /*html*/`
+            <button onclick="activateChoosenGen(252, 386)">Gen3</button>
+        `
+    }
+    if(i === 493){
+        GenButtonContainer.innerHTML += /*html*/`
+            <button onclick="activateChoosenGen(387, 493)">Gen4</button>
+        `
+    }
+    if(i === 649){
+        GenButtonContainer.innerHTML += /*html*/`
+            <button onclick="activateChoosenGen(494, 649)">Gen5</button>
+        `
+    }
+    if(i === 721){
+        GenButtonContainer.innerHTML += /*html*/`
+            <button onclick="activateChoosenGen(650, 721)">Gen6</button>
+        `
+    }
+    if(i === 809){
+        GenButtonContainer.innerHTML += /*html*/`
+            <button onclick="activateChoosenGen(722, 809)">Gen7</button>
+        `
+    }
+    if(i === 905){
+        GenButtonContainer.innerHTML += /*html*/`
+            <button onclick="activateChoosenGen(810, 905)">Gen8</button>
+        `
+    }
+    if(i === 1010){
+        GenButtonContainer.innerHTML += /*html*/`
+            <button onclick="activateChoosenGen(906, 1010)">Gen9</button>
+        `
+    }
+}
+
 
 function buildCard(JSON){
     let name = JSON['name'];
@@ -22,11 +70,76 @@ function buildCard(JSON){
     pokemonCards.innerHTML +=/*html*/`
     <div id=${name} class="pokemonCard">
         <div id="name${name}" class="pokemonName">${name}</div>
-        <div id="id${name}">#${id}</div>
+        <div id="${id}">#${id}</div>
         <div id="picture${name}"><img src="${picture}"></div>
     </div>
-    `    
+    `
+    activateFirstGen(id, name);
 }
+
+
+function activateFirstGen(id, name){
+
+    let pokemon = document.getElementById(`${name}`);
+    if(id <=151){
+        pokemon.classList.add('d-flex');
+    }
+}
+
+
+function activateChoosenGen(i, j){  
+
+    deletePokemonCardsOver(j);
+    deletePokemonCardsUnder(i); 
+    for (i; i <= j; i++) {
+        const id = i;
+        let idPokemon = document.getElementById(`${id}`);
+        let parent = idPokemon.parentElement;
+        console.log(parent);
+        if(i <= id && id <= j){
+            parent.classList.remove('d-none');
+            parent.classList.add('d-flex');
+        }        
+    } 
+}
+
+
+function deletePokemonCardsOver(j){
+    let jdruber = j + (1010-j);
+
+    for (jdruber; jdruber < 1010 ; jdruber++) {
+        const idEraseToo = jdruber;
+        let idPokemon = document.getElementById(`${idEraseToo}`);
+        let parento = idPokemon.parentElement;
+        console.log(parent);
+            parento.classList.remove('d-flex');
+            parento.classList.add('d-none');    
+    }
+}
+
+function deletePokemonCardsUnder(i){
+
+    let idrunter = i -(i-1);
+
+    for (idrunter; idrunter < i; idrunter++) {
+        const idErase = idrunter;
+        let idPokemon = document.getElementById(`${idErase}`);
+        let parent = idPokemon.parentElement;
+
+        parent.classList.remove('d-flex');
+        parent.classList.add('d-none');
+    }
+}
+
+
+function buildLoadingDisplay(i){
+    ;
+    document.getElementById('loading').innerHTML =/*html*/`
+        ${i} pokemon of 1010 loaded
+    `
+    loadGenButton(i);
+}
+
 
 
 function createloadFiftyMoreBtn(loadedAmount){
--- scriptTemplates.js
+++ scriptTemplates.js
@@ -2,6 +2,10 @@ function loadPokedex(){
     let pokedex = document.getElementById('pokedex');
     pokedex.innerHTML = /*html*/`
         <h1>Pokedex</h1>
+        <div id="genButtonContainer">
+            <button onclick="activateChoosenGen(1, 151)">Gen1</button>
+        </div>
+        <div id="loading"></div>
         <div id="searchPokemon">
             <input id="searchText" type="text" onkeyup="searchPokemon()" placeholder="Search Pokemon">
         </div>
@@ -12,6 +16,6 @@ function loadPokedex(){
             <div id="loadTwoHundredMore"></div>
         </div>
     `
-    loadPokemon(1, 100);
+    loadPokemon();
 }
 
